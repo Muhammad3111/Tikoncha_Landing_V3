2ae4d0d900b927ed936891867b619ff6724e2304
Muhammad3111 <karimovmuhammad3502@gmail.com>
2026-04-30 14:39:51 +0500

    add Meta Pixel tracking script to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Golos_Text } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { siteData } from "@/lib/site-data";
 
@@ -36,6 +37,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="uz" suppressHydrationWarning>
+      <head>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window,document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '2320283435165290');
+          fbq('track', 'PageView');`}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2320283435165290&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+      </head>
       <body suppressHydrationWarning className={`${golos.variable} font-sans text-mist`}>
         {children}
       </body>
